badb9333dc15663033c5a61d314211d3950e0aa1
0xDarkStar <111785550+0xDarkStar@users.noreply.github.com>
2024-03-06 22:42:43 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,5 @@
+// Main plan: Make a simple music playing app
+// It will look in a folder that holds all the music
+// Make folders for different playlists (Download mp3/mp4 of music [and subtitles if wanted] then put into a folder with song name and artist [ex: Tenellous | Jeremy Blake])
+// Allow the user to view both video and subtitles (if possible)
+// And be able to download more music (using services like yt-dlp or others that I don't know of) (Those services will have to be downloaded though...)
