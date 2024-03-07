d0ce76d0e8af63761a132253af62026f81edeb5f
0xDarkStar <mattrojas.email@gmail.com>
2024-03-07 00:28:19 -0500

    Still needs to be tested, will update my launch command to allow options

--- musicPlayer/main.cpp
+++ musicPlayer/main.cpp
@@ -1,8 +1,19 @@
 // Made by: 0xDarkStar
 #include <iostream>
+#include <sqlite3.h>
 using namespace std;
 
 int main() {
-    cout << "Hello World"; // Placeholder
+    sqlite3* DB;
+    int exit = 0;
+    exit = sqlite3_open("song.db", &DB);
+
+    if (exit) {
+        cerr << "Error open DB" << sqlite3_errmsg(DB) << endl;
+        return -1;
+    }
+    else
+        cout << "Opened Database Successfully!" << endl;
+    sqlite3_close(DB);
     return 0;
 }
