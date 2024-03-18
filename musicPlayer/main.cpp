5e4612c29e12a76a59df4b5e40af8ea08eeb8ed0
0xDarkStar <mattrojas.email@gmail.com>
2024-03-17 22:23:46 -0400

    Moved a few things. Can now see how many total songs there are. Did more as well

--- musicPlayer/main.cpp
+++ musicPlayer/main.cpp
@@ -7,8 +7,16 @@ using namespace std::filesystem;
 
 int main() {
     int output = 0;
-    // output = create_table();
-    output = populate_songs_table(1, "Title", "Artist", false, 255);
-    output = delete_song(1);
+    // output = create_tables();
+    // output = add_song_to_table(2, "Title", "Artist", false, 255);
+    // output = delete_song(1);
+    // for (int i = 0; i < 10; i++) {
+    //     add_song_to_table(i, "Title", "Artist", false, 255);
+    // }
+    // for (int i = 0; i < 10; i++) {
+    //     delete_song(i);
+    // }
+    // output = read_table("songs");
+    output = get_table_length("songs");
     return output;
 }
--- musicPlayer/sqlCommands.h
+++ musicPlayer/sqlCommands.h
@@ -1,17 +1,62 @@
 #include <iostream>
 #include <sqlite3.h>
+#include <typeinfo>
 
-int create_table() { // FYI, all of this is from geeksforgeeks.org/sql-using-c-c-and-sqlite
+/* Callbacks for reading the table and its items
+(These comments outside will be for separating sections) */
+static int print_table_items(void* data, int argc, char** argv, char** azColName) {
+    int i;
+    fprintf(stderr, "%s: ", (const char*)data);
+
+    for (i = 0; i < argc; i++) {
+        printf("%s", argv[i] ? argv[i] : "NULL");
+        if (i != argc-1) {
+            printf(" | ");
+        }
+    }
+    printf("\n");
+    return 0;
+}
+
+extern "C" int return_table_length(void* data, int argc, char** argv, char** azColName) {
+    /*
+    This code is from
+    https://stackoverflow.com/questions/20959338/storing-result-of-count-query-into-a-c-integer-in-sqlite
+    */
+    if (argc == 1 && argv) {
+        *static_cast<int*>(data) = atoi(argv[0]);
+        return 0;
+    }
+    return 1;
+}
+
+/* Making of all tables (should be a one-time thing) */
+int create_tables() { // FYI, all of this is from geeksforgeeks.org/sql-using-c-c-and-sqlite
     sqlite3* DB; // Of course it's modified a bit to work for me.
-    std::string sql = "CREATE TABLE Songs("
+    std::string sql = "CREATE TABLE songs("
+                 "ID INT NOT NULL, "
+                 "Title TEXT NOT NULL, "
+                 "Artist TEXT, "
+                 "Has_subs INT NOT NULL, "
+                 "Length INT NOT NULL,"
+                 "PRIMARY KEY (ID) );"; // Make table command
+    std::string sql1 = "CREATE TABLE playlists("
                  "ID             INT      NOT NULL, "
                  "Title          TEXT     NOT NULL, "
-                 "Artist         TEXT, "
-                 "Has_subs       INT      NOT NULL, "
-                 "Length         INT      NOT NULL,"
-                 "PRIMARY KEY (ID) );"; // Make table command
+                 "Length         INT      NOT NULL, "
+                 "Num_of_songs   INT      NOT NULL, "
+                 "PRIMARY KEY (ID) );";
+    std::string sql2 = "CREATE TABLE songSubs("
+                 "Song_ID        INT     NOT NULL, "
+                 "Sub_ID         INT     NOT NULL, "
+                 "Language       INT     NOT NULL, "
+                 "PRIMARY KEY (Song_ID) );";
+    std::string sql3 = "CREATE TABLE songPlaylists("
+                 "Song_ID        INT      NOT NULL, "
+                 "Playlist_ID    INT      NOT NULL, "
+                 "PRIMARY KEY (Song_ID) );";
     int exit = 0;
-    exit = sqlite3_open("main.db", &DB); // Open the database
+    sqlite3_open("main.db", &DB); // Open the database
     char* messaggeError;
     exit = sqlite3_exec(DB, sql.c_str(), NULL, 0, &messaggeError); // Attempt to make a table
     if (exit != SQLITE_OK) { // Table wasn't made
@@ -24,32 +69,19 @@ int create_table() { // FYI, all of this is from geeksforgeeks.org/sql-using-c-c
     return 0;
 }
 
-static int callback(void* data, int argc, char** argv, char**azColName) {
-    int i;
-    fprintf(stderr, "%s: ", (const char*)data);
-
-    for (i = 0; i < argc; i++) {
-        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-    }
-    printf("\n");
-    return 0;
-}
-
-int populate_songs_table(int id, std::string title, std::string artist, bool has_subs, int length) { // length is in seconds
+/* Messing with the contents of the tables (adding/removing values) */
+int add_song_to_table(int id, std::string title, std::string artist, bool has_subs, int length) { // length is in seconds
     sqlite3* DB;
     char* messaggeError;
     int exit = 0;
-    std::string sql = "INSERT INTO Songs VALUES(" +
-                 std::to_string(id) + ", '" +
+    std::string sql = "INSERT INTO songs VALUES(" +
+                 std::to_string(id) + ", '" + // Read max ID in songs then change this ID to +1 the max
                  title + "', '" +
                  artist + "', ";
     sql +=       (has_subs) ? "true":"false";
     sql +=       ", " + std::to_string(length) + ");"; // Add song to table command
-    
-    std::string query = "SELECT * FROM Songs;";
-    std::cout << "State of table before insert" << std::endl;
-    exit = sqlite3_open("main.db", &DB);
-    sqlite3_exec(DB, query.c_str(), callback, NULL, NULL); // Check all songs
+
+    sqlite3_open("main.db", &DB);
     exit = sqlite3_exec(DB, sql.c_str(), NULL, 0, &messaggeError); // Attempt to add song
     if (exit != SQLITE_OK) {
         std::cerr << "Error adding values" << std::endl;
@@ -57,9 +89,11 @@ int populate_songs_table(int id, std::string title, std::string artist, bool has
     }
     else
         std::cout << "Values added successfully" << std::endl;
-    std::cout << "State of table after insert" << std::endl;
-    sqlite3_exec(DB, query.c_str(), callback, NULL, NULL);
     sqlite3_close(DB);
+    return 0;
+}
+
+int create_playlist(int id) {
     return 0;
 }
 
@@ -67,8 +101,8 @@ int delete_song(int id) {
     sqlite3* DB;
     char* messaggeError;
     int exit = 0;
-    std::string sql = "DELETE FROM Songs WHERE ID = " + std::to_string(id) + ";"; // Delete song from table command
-    exit = sqlite3_open("main.db", &DB);
+    std::string sql = "DELETE FROM songs WHERE ID = " + std::to_string(id) + ";"; // Delete song from table command
+    sqlite3_open("main.db", &DB);
     exit = sqlite3_exec(DB, sql.c_str(), NULL, 0, &messaggeError); // Attempt to delete song
     if (exit != SQLITE_OK) {
         std::cerr << "Error deleting song" << std::endl;
@@ -80,10 +114,30 @@ int delete_song(int id) {
     return 0;
 }
 
-int read_table(char* table) {
+/* Looking at the table and its items */
+int read_table(std::string table) {
     sqlite3* DB;
     int exit = 0;
-    std::string sql = ""; // To be continued...
+    std::string sql = "SELECT * FROM " + table + ";"; // To be continued...
+    sqlite3_open("main.db", &DB);
+    std::cout << "Items in " + table << std::endl;
+    std::cout << "ID | Title | Artist | Has_subs | Length" << std::endl;
+    sqlite3_exec(DB, sql.c_str(), print_table_items, NULL, NULL);
     sqlite3_close(DB);
+    return 0;
+}
+
+int get_table_length(std::string table) {
+    sqlite3* DB;
+    int length;
+    int exit;
+    sqlite3_open("main.db", &DB);
+    std::string sql = "SELECT COUNT(ID) FROM " + table + ";";
+    exit = sqlite3_exec(DB, sql.c_str(), return_table_length, &length, NULL);
+    if (exit != SQLITE_OK) {
+        std::cerr << "Error reading table" << std::endl;
+    }
+    else
+        std::cout << "Length of " << table << " is " << length << std::endl;
     return 0;
 }
