13e0fece1910b73c03dddfd0b1c73b85e3733c21
0xDarkStar <111785550+0xDarkStar@users.noreply.github.com>
2025-10-14 14:32:17 -0400

    Add logic for Flow style

--- BinPacker/formatter/Formatter.java
+++ BinPacker/formatter/Formatter.java
@@ -1,5 +1,6 @@
 package formatter;
 import java.util.List;
+import java.util.Map;
 
 import calculator.Run;
 import calculator.RunItem;
@@ -20,21 +21,124 @@ public class Formatter {
         }
     }
 
-    public void writeRunsToFile(String outputType, List<Run> runs) {
+    public void writeRunsToFile(String outputType, List<Run> runs, List<Map.Entry<String,Integer>> fullList) {
         switch (outputType) {
             case "flow":
-                writeFlowRuns(runs);
+                writeFlowRuns(runs, fullList);
                 break;
             case "block":
                 writeBlockRuns(runs);
+                break;
             default:
                 writeBlockRuns(runs);
                 break;
         }
     }
 
-    public void writeFlowRuns(List<Run> runs) {
+    public void writeFlowRuns(List<Run> runs, List<Map.Entry<String,Integer>> fullList) {
+        int itemIndex = 0;
+        boolean currIsRemainder = false;
+        int nextRun = 1;
+        int spacesAdded = 0;
+        int maxNameSize = 0;
+        int maxNumSize = 0;
+        // Determine the max sizes for names and numbers
+        for (Run run : runs) {
+            if (run.getLargestNameSize() > maxNameSize) {
+                maxNameSize = run.getLargestNameSize();
+            }
+            if (run.getNumSize() > maxNumSize) {
+                maxNumSize = run.getNumSize();
+            }
+        }
 
+        String printedList = "";
+        for (Run currRun : runs) {
+            int currRunSize = currRun.getItems().size();
+            int halfSize = currRunSize/2;
+            for (int i = 0; i < currRunSize; i++) {
+                RunItem item = currRun.getItems().get(i);
+                if (currIsRemainder || !item.getName().equals(fullList.get(itemIndex).getKey())) {
+                    // This is an item we already covered, SKIP!
+                    continue;
+                }
+
+                // Add the item and its info
+                printedList += String.format(" - %-"+maxNameSize+"s %"+maxNumSize+"d ", item.getName(), fullList.get(itemIndex).getValue());
+                // Add the indentation needed
+                printedList += " ".repeat(spacesAdded);
+
+                // Is it a pyramid or not?
+                if (currRunSize > 2) { // Yes
+                    // Figure out where on the pyramid we are
+                    if (i < halfSize) { // We're near the start
+                        printedList += "\\";
+                        spacesAdded++;
+                    } else if (i == halfSize) { // We're where we see the run data
+                        printedList += "> Run "+currRun.getRunStartNumber()+" ("+currRun.getTotalUsed()+")";
+                        spacesAdded--;
+                    } else if (i == currRunSize-1 && currRun.getRunRemainder() > 0) {
+                        // This only runs if we're at the end of the pyramid and we have a remainder
+                        printedList += "/ \\ (remainder: "+currRun.getRunRemainder()+")";
+                        spacesAdded += 3;
+                    } else { // We're at the end of the pyramid. We don't have any remainders either!
+                        printedList += "/";
+                        spacesAdded--;
+                    }
+                } else if (currRunSize == 2) { // Not really
+                    // Always make the second item the one with the run info
+                    if (i == 1 && currRun.getRunRemainder() > 0) { // We've got a remainder, let it flow to the next one
+                        printedList += "\\ Run "+currRun.getRunStartNumber()+" ("+currRun.getTotalUsed()+") (remainder: "+currRun.getRunRemainder()+")";
+                        spacesAdded++;
+                    } else if (i == 1) { // No remainder, we can cut the slope here
+                        printedList += "> Run "+currRun.getRunStartNumber()+" ("+currRun.getTotalUsed()+")";
+                        spacesAdded = 0;
+                    } else { // We're the first item.
+                        printedList += "\\";
+                        spacesAdded++;
+                    }
+                } else { // No
+                    // What type of FULL run are we?
+                    if (runs.get(nextRun).getRunRemainder() > 0 && currRun.isFull() && !runs.get(nextRun).isFull()) {
+                        // FULL run with REMAINDER run that also leaves a remainder
+                        printedList += "\\ Run "+currRun.getRunStartNumber()+"-"+(currRun.getRunEndNumber()+1)+" ("+currRun.getTotalUsed()+") ("+runs.get(nextRun).getTotalUsed()+") (remainder: "+runs.get(nextRun).getRunRemainder()+")";
+                        spacesAdded++;
+                    } else if (runs.get(nextRun).getRunRemainder() == 0 && currRun.isFull() && !runs.get(nextRun).isFull()) {
+                        // FULL run with REMAINDER run and NO remainder
+                        printedList += "> Run "+currRun.getRunStartNumber()+"-"+(currRun.getRunEndNumber()+1)+" ("+currRun.getTotalUsed()+") ("+runs.get(nextRun).getTotalUsed()+")";
+                        spacesAdded = 0;
+                    } else if (currRun.getRunRemainder() > 0 && currRun.isFull()) {
+                        // FULL run with a remainder
+                        printedList += "\\ Run "+currRun.getRunStartNumber()+"-"+currRun.getRunEndNumber()+" ("+currRun.getTotalUsed()+") (remainder: "+currRun.getRunRemainder()+")";
+                        spacesAdded++;
+                    } else if (currRun.getRunRemainder() == 0 && currRun.isFull()) {
+                        // Full run with NO remainder
+                        printedList += "> Run "+currRun.getRunStartNumber()+"-"+currRun.getRunEndNumber()+" ("+currRun.getTotalUsed()+")";
+                        spacesAdded = 0;
+                    }
+                }
+                // Moving to next item
+                printedList += "\n"; 
+                itemIndex++;
+                // Prevent IndexOutOfBounds errors:
+                if (itemIndex == fullList.size()) {
+                    itemIndex = 0;
+                }
+                if (spacesAdded < 0) {
+                    spacesAdded = 0;
+                }
+            }
+            nextRun++;
+        }
+        try {
+            writer = new FileWriter("FlowRuns.txt");
+            writer.write(printedList);
+            writer.close();
+        } catch (IOException e) {
+            System.err.println("We recieved an error while trying to write to the file.");
+            System.err.println(e);
+            System.exit(0);
+        }
     }
 
     public void writeBlockRuns(List<Run> runs) {
@@ -80,7 +184,9 @@ public class Formatter {
             }
             writer.close();
         } catch (IOException e) {
-            // TODO: handle exception
+            System.err.println("We recieved an error while trying to write to the file.");
+            System.err.println(e);
+            System.exit(0);
         }
     }
 }
